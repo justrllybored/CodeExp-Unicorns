bc72a185a1f4de9ae39b01fe583c34d99aa1e558
tancysam <80210931+tancysam@users.noreply.github.com>
2021-06-15 11:25:30 +0800

    Adding files not folders

---
+++ App.js
@@ -0,0 +1,48 @@
+import * as React from "react";
+import { Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import MapScreen from "./Screens/MapScreen";
+import ProfileScreen from "./Screens/ProfileScreen";
+import { FontAwesome } from "@expo/vector-icons";
+
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            //Set the icon based on which route it is (name of the tab)
+            if (route.name === "Map") {
+              iconName = focused ? "map" : "map-o";
+            } else if (route.name === "Profile") {
+              iconName = focused ? "user" : "user-o";
+            }
+
+            // You can return any component that you like here!
+            return <FontAwesome name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: "tomato",
+          inactiveTintColor: "gray",
+        }}
+      >
+        <Tab.Screen name="Map" component={MapScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
