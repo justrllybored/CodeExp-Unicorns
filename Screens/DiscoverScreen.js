cd6426b0e93bb155a69cc8ad9a51e331d70e633d
FailerzJ <zhengjiatan123@gmail.com>
2021-06-15 22:20:38 +0800

    commit

--- Screens/DiscoverScreen.js
+++ Screens/DiscoverScreen.js
@@ -21,13 +21,33 @@ function DiscoverScreen({ navigation }) {
         onPress={() => navigation.navigate("DiscoverOne")}
         style={styles.DiscoverOne}
       >
-        <Text>Something for now</Text>
+        <Text style={styles.textTitleOne}>Title</Text>
+        <Text style={styles.textDescriptionOne}>Description</Text>
+        <Text style={styles.textRatingOne}>
+          Ratings:
+          <FontAwesome name="star" size={24} color="black" />
+          <FontAwesome name="star" size={24} color="black" />
+          <FontAwesome name="star" size={24} color="black" />
+          <FontAwesome name="star" size={24} color="black" />
+          <FontAwesome name="star-half-full" size={24} color="black" />
+        </Text>
+        <Text style={styles.textPeopleOne}>People at location: 69</Text>
       </TouchableOpacity>
       <TouchableOpacity
         onPress={() => navigation.navigate("DiscoverTwo")}
         style={styles.DiscoverTwo}
       >
-        <Text>Something for now</Text>
+        <Text style={styles.textTitleTwo}>Title</Text>
+        <Text style={styles.textDescriptionTwo}>Description</Text>
+        <Text style={styles.textRatingTwo}>
+          Ratings:
+          <FontAwesome name="star" size={24} color="black" />
+          <FontAwesome name="star" size={24} color="black" />
+          <FontAwesome name="star" size={24} color="black" />
+          <FontAwesome name="star" size={24} color="black" />
+          <FontAwesome name="star-o" size={24} color="black" />{" "}
+        </Text>
+        <Text style={styles.textPeopleTwo}>People at location: 126</Text>
       </TouchableOpacity>
     </View>
   );
@@ -65,8 +85,6 @@ const styles = StyleSheet.create({
   DiscoverOne: {
     backgroundColor: "lightblue",
     width: "90%",
-    justifyContent: "center",
-    alignItems: "center",
     alignSelf: "center",
     padding: 10,
     borderRadius: 20,
@@ -77,12 +95,65 @@ const styles = StyleSheet.create({
   DiscoverTwo: {
     backgroundColor: "lightblue",
     width: "90%",
-    justifyContent: "center",
-    alignItems: "center",
     alignSelf: "center",
     padding: 10,
     borderRadius: 20,
     height: 300,
     margin: 10,
+  },
+  textTitleOne: {
+    fontWeight: "bold",
+    fontSize: 30,
+    textAlign: "left",
+    marginTop: 20,
+    marginLeft: 20,
+  },
+  textDescriptionOne: {
+    marginTop: 20,
+    marginLeft: 20,
+    fontSize: 15,
+  },
+  textRatingOne: {
+    textAlign: "left",
+    marginTop: 120,
+    marginLeft: 20,
+    fontSize: 25,
+  },
+
+  textPeopleOne: {
+    textAlign: "left",
+    marginBottom: 20,
+    marginLeft: 20,
+    marginTop: 8,
+    fontSize: 20,
+  },
+  textTitleTwo: {
+    fontWeight: "bold",
+    fontSize: 30,
+    textAlign: "left",
+    marginTop: 20,
+    marginLeft: 20,
+  },
+  textDescriptionTwo: {
+    marginTop: 20,
+    marginLeft: 20,
+    fontSize: 15,
+  },
+  textRatingTwo: {
+    textAlign: "left",
+    marginTop: 120,
+    marginLeft: 20,
+    fontSize: 25,
+  },
+  iconRatingTwo: {
+    marginLeft: 20,
+    marginTop: 5,
+  },
+  textPeopleTwo: {
+    textAlign: "left",
+    marginBottom: 20,
+    marginLeft: 20,
+    marginTop: 8,
+    fontSize: 20,
   },
 });
