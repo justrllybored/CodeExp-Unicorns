c4aa4607a737544e01d3bce51239132991a04f57
AngelLXY <85489571+AngelLXY@users.noreply.github.com>
2021-06-16 02:51:35 +0800

    Update ProfileScreen.js

--- Screens/ProfileScreen.js
+++ Screens/ProfileScreen.js
@@ -1,15 +1,23 @@
 import * as React from "react";
-import { Text, View, TouchableOpacity } from "react-native";
+import { Text, View, TouchableOpacity, StyleSheet } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 
 function ProfileScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Profile!</Text>
+    <View style={styles.container}>
+      <Text>Profile</Text>
+      <TouchableOpacity onPress={onPress}></TouchableOpacity>
     </View>
   );
 }
 
+const styles= StyleSheet.create({
+    container:{
+     flex: 1, 
+     justifyContent: "center",
+     alignItems: "center" 
+    }
+})
 const Stack = createStackNavigator();
 
 export default function ProfileStack() {
@@ -19,33 +27,3 @@ export default function ProfileStack() {
     </Stack.Navigator>
   );
 }
-
-// import * as React from "react";
-// import { Text, View, TouchableOpacity, StyleSheet } from "react-native";
-// import { createStackNavigator } from "@react-navigation/stack";
-
-// function ProfileScreen() {
-//   return (
-//     <View style={styles.container}>
-//       <Text>Profile</Text>
-//       <TouchableOpacity onPress={onPress}></TouchableOpacity>
-//     </View>
-//   );
-// }
-
-// const styles= StyleSheet.create({
-//     container:{
-//      flex: 1, 
-//      justifyContent: "center",
-//      alignItems: "center" 
-//     }
-// })
-// const Stack = createStackNavigator();
-
-// export default function ProfileStack() {
-//   return (
-//     <Stack.Navigator>
-//       <Stack.Screen name="Profile" component={ProfileScreen} />
-//     </Stack.Navigator>
-//   );
-// }
