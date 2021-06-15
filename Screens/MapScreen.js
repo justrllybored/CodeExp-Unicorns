922e2fb2972ebbee3f5f08eab7a8b872f4eb7326
AngelLXY <85489571+AngelLXY@users.noreply.github.com>
2021-06-16 02:48:19 +0800

    Update MapScreen.js

--- Screens/MapScreen.js
+++ Screens/MapScreen.js
@@ -19,3 +19,34 @@ export default function MapStack() {
     </Stack.Navigator>
   );
 }
+
+
+// import * as React from "react";
+// import { Text, View, TouchableOpacity, StyleSheet } from "react-native";
+// import { createStackNavigator } from "@react-navigation/stack";
+
+// function ProfileScreen() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Map</Text>
+//       <TouchableOpacity onPress={onPress}></TouchableOpacity>
+//     </View>
+//   );
+// }
+
+// const styles= StyleSheet.create({
+//     container:{
+//      flex: 1, 
+//      justifyContent: "center",
+//      alignItems: "center" 
+//     }
+// })
+// const Stack = createStackNavigator();
+
+// export default function ProfileStack() {
+//   return (
+//     <Stack.Navigator>
+//       <Stack.Screen name="Map" component={ProfileScreen} />
+//     </Stack.Navigator>
+//   );
+// }
